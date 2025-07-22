e139ec8c980ac584c8c95df644c7faed7cc6bf28
yeisonmenau <yeisonandresdh@gmail.com>
2025-07-22 10:37:00 -0500

    Crear DeudaUseCase

---
+++ src/main/java/com/yeisonmenau/gestordeudas/application/usecase/DeudaUseCase.java
@@ -0,0 +1,13 @@
+package com.yeisonmenau.gestordeudas.application.usecase;
+
+import com.yeisonmenau.gestordeudas.domain.deuda.model.Deuda;
+
+import java.util.List;
+
+public interface DeudaUseCase {
+    Deuda crearDeuda (Deuda deuda);
+    List<Deuda> mostrarDeudas ();
+    Deuda actualizarDeuda (Long idDeuda, Deuda deuda);
+    String eliminarDeuda (Long idDeuda);
+    String saldarDeuda (Long idDeuda);
+}
--- src/main/java/com/yeisonmenau/gestordeudas/infrastructure/adapter/implement/DeudaAdapter.java
+++ src/main/java/com/yeisonmenau/gestordeudas/infrastructure/adapter/implement/DeudaAdapter.java
@@ -4,6 +4,8 @@ import com.yeisonmenau.gestordeudas.domain.deuda.model.Deuda;
 import com.yeisonmenau.gestordeudas.domain.deuda.out.DeudaRepository;
 import com.yeisonmenau.gestordeudas.infrastructure.adapter.DeudaJpaRepository;
 import com.yeisonmenau.gestordeudas.infrastructure.adapter.PersonaJpaRepository;
+import com.yeisonmenau.gestordeudas.infrastructure.entity.DeudaEntity;
+import com.yeisonmenau.gestordeudas.infrastructure.mapper.DeudaMapper;
 import com.yeisonmenau.gestordeudas.infrastructure.mapper.PersonaMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,11 +17,13 @@ public class DeudaAdapter implements DeudaRepository {
 
     private final DeudaJpaRepository deudaJpaRepository;
     private final PersonaJpaRepository personaJpaRepository;
-    private final PersonaMapper mapper;
+    private final DeudaMapper mapper;
 
     @Override
     public Deuda crearDeuda(Deuda deuda) {
-        return null;
+        DeudaEntity deudaEntidad = mapper.domainToEntity(deuda);
+        DeudaEntity deudaGuardada = deudaJpaRepository.save(deudaEntidad);
+        return mapper.entityToDomain(deudaGuardada);
     }
 
     @Override
